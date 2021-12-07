16224efb2596aa21c193a5e7e53eb36605e35156
Bruna Lika <brunalika@msn.com>
2021-12-07 15:37:48 -0300

    criacao de widgets

--- 01-FundFlutter/bytebank/lib/main.dart
+++ 01-FundFlutter/bytebank/lib/main.dart
@@ -1,17 +1,66 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(Column(
+void main() => runApp(MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      //Aplicacao de tecnicas de extração de codigos
+      body: ListaTransferencia(),
+    )));
+
+/*
+Refatorações aplicadas seguindo as boas praticas de programacao
+apresentadas
+*/
+
+/*
+A diferença mais importante é que o stateless é um widget estático, ou seja, 
+não temos como gerenciar o estado dele. E o stateful é completamente dinâmico 
+e nos dá o poder de torná-lo mutável através da gerência de estados.
+https://www.alura.com.br/artigos/flutter-diferenca-entre-stateless-e-statefull-widget
+*/
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
       children: <Widget>[
-        Text('Texto um', textDirection: TextDirection.ltr),
-        Text('Texto dois', textDirection: TextDirection.ltr),
-        Expanded(
-          child: FittedBox(
-            fit: BoxFit.contain,
-            child: FlutterLogo(),
-          ),
-        ),
-        Column(children: <Widget>[
-          Text('Texto tres', textDirection: TextDirection.ltr),
-        ]),
+        ItemTransferencia(Transferencia(100.00, 123)),
+        ItemTransferencia(Transferencia(200.00, 456)),
+        ItemTransferencia(Transferencia(300.00, 789)),
       ],
+    );
+  }
+}
+
+//Quebra dos componentes em menores partes
+class ItemTransferencia extends StatelessWidget {
+  Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      leading: Icon(Icons.monetization_on),
+      title: Text(_transferencia.valor.toString()),
+      subtitle: Text(_transferencia.numeroConta.toString()),
     ));
+  }
+}
+
+//Classe para auxiliar no significado do que esta sendo realizado
+class Transferencia {
+  late final double valor;
+  late final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
